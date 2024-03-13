9002b6b7445a1c559075eefc4a28b6199749f2b9
Nikita Sytnik <quazzz13@gmail.com>
2024-03-13 12:04:00 +0100

    first

---
+++ script.js
@@ -0,0 +1,9 @@
+function showNavlist(){
+    const navlist = document.querySelector('.nav__list-mobile')
+    
+    if (navlist.style.display = 'none') {
+        navlist.style.display = 'flex'
+    } else if (navlist.style.display = 'flex') {
+        navlist.style.display = 'none'
+    }
+}
