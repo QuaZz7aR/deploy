7da1ef612fc6bfac2077ee232953c7423d8e2f88
Nikita Sytnik <quazzz13@gmail.com>
2024-03-13 13:57:09 +0100

    final

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-function showNavlist(){
-    const navlist = document.querySelector('.nav__list-mobile')
+const hamburger = document.querySelector('.hamburger');
+const navlist = document.querySelector('.nav__list-mobile')
     
-    if (navlist.style.display = 'none') {
-        navlist.style.display = 'flex'
-    } else if (navlist.style.display = 'flex') {
-        navlist.style.display = 'none'
-    }
-}
+hamburger.addEventListener("click", () => {
+    navlist.classList.toggle("active");
+})
+
+document.querySelectorAll('.mobile__link').forEach(n => n.
+    addEventListener("click", () => {
+        navlist.classList.remove("active");
+    }))
